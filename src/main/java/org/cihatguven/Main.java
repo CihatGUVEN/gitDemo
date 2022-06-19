d2857fbd292005f0b0ee1026bdce5331e8a26826
CihatGUVEN <89444171+CihatGUVEN@users.noreply.github.com>
2022-06-19 14:11:18 +0300

    first commit

--- src/main/java/org/cihatguven/Main.java
+++ src/main/java/org/cihatguven/Main.java
@@ -1,4 +1,6 @@
-package org.cihatguven;public class Main {
+package org.cihatguven;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
